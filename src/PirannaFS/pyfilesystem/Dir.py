e9ba1a3042c961ab4b2b2745004932231f6378de
Jesús Leganés Combarro "Piranna <piranna@gmail.com>
2011-03-15 23:33:00 +0000

    Documentation of Dir class

--- src/PirannaFS/pyfilesystem/Dir.py
+++ src/PirannaFS/pyfilesystem/Dir.py
@@ -7,7 +7,9 @@ Created on 15/08/2010
 import os
 import stat
 
-from fs.errors import *
+from fs.errors import DestinationExistsError, DirectoryNotEmptyError
+from fs.errors import ParentDirectoryMissingError
+from fs.errors import ResourceError, ResourceInvalidError
 
 import plugins
 
@@ -18,15 +20,22 @@ class Dir:
     '''
 
     def __init__(self, fs, path):                                              # OK
-        '''
-        Constructor
-        '''
+        """Constructor
+
+        @param fs: reference to the filesystem instance
+        @param path: path of this dir
+        @type path: string
+
+        :raises ParentDirectoryMissingError: if a containing directory is missing
+        :raises ResourceInvalidError:        if the path exists, but is not a directory
+        :raises ResourceNotFoundError:       if the path is not found
+        """
         try:
             self.__inode = fs.Get_Inode(path[1:])
         except ResourceError:
             self.__inode = None
         else:
-            # Inode is not from a dir
+            # If inode is not of a dir, raise error
             if fs.db.Get_Mode(self.__inode) != stat.S_IFDIR:
                 raise ResourceInvalidError(path)
 
@@ -34,9 +43,22 @@ class Dir:
         self.path = path
 
     def isempty(self):
+        """Check if a directory is empty (contains no files or sub-directories)
+
+        @rtype: bool
+        """
         return self.fs.db.readdir(self.__inode, 1)
 
     def make(self, recursive=False, allow_recreate=False):
+        """Make a directory on the filesystem.
+
+        @param recursive: if True, any intermediate directories will also be created
+        @type recursive: bool
+        @param allow_recreate: if True, re-creating a directory wont be an error
+        @type allow_create: bool
+
+        :raises DestinationExistsError: if the path is already a directory, and allow_recreate is False
+        """
         # Check if dir_entry exist and we can recreate it if so happens
         if self.__inode and not allow_recreate:
             raise DestinationExistsError(self.path)
@@ -60,8 +82,15 @@ class Dir:
         self.__inode = self.db.mkdir()
         self.db.link(parent_dir_inode, name, self.__inode)
 
-    def read(self):                                                          # OK
-        plugins.send("Dir.read start")
+        plugins.send("Dir.make")
+
+    def list(self):                                                          # OK
+        """Lists the files and directories under a given path.
+        The directory contents are returned as a list of unicode paths.
+
+        @rtype: iterable of paths
+        """
+        plugins.send("Dir.list begin")
 
 #        yield unicode('.')
 #        yield unicode('..')
@@ -70,9 +99,18 @@ class Dir:
             if dir_entry['name']:
                 yield unicode(dir_entry['name'])
 
-        plugins.send("Dir.read end")
+        plugins.send("Dir.list end")
 
     def remove(self, recursive=False, force=False):
+        """Remove a directory from the filesystem
+
+        @param recursive: if True, then empty parent directories will be removed
+        @type recursive: bool
+        @param force: if True, any directory contents will be removed
+        @type force: bool
+
+        @raises DirectoryNotEmptyError: if the directory is not empty and force is False
+        """
         # Force dir deletion
         if force:
             for dir_entry in self.fs.db.readdir(self.__inode):
@@ -102,3 +140,5 @@ class Dir:
                 d.remove(True)
             except DirectoryNotEmptyError:
                 pass
+
+        plugins.send("Dir.remove")
