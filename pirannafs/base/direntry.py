7ceddb94cd0a329911ec710ff7b92d8fe2779042
Jesús Leganés Combarro "Piranna" <piranna@gmail.com>
2012-07-01 18:47:11 +0200

    Created Inode class

--- pirannafs/base/direntry.py
+++ pirannafs/base/direntry.py
@@ -10,30 +10,19 @@ from pirannafs.errors import ParentDirectoryMissing
 from pirannafs.errors import ResourceNotFound
 
 
-class DirEntry(object):
+class Inode(object):
     '''
     classdocs
     '''
 
-    def __init__(self, fs):
+    def __init__(self, fs, inode):
         '''
         Constructor
         '''
         self.fs = fs
         self.db = fs.db
 
-        self.parent, self.name = split(self.path)
-
-        try:
-            self.parent = fs._Get_Inode(self.parent)
-        except (ParentDirectoryMissing, ResourceNotFound):
-            self._inode = None
-            return
-
-        try:
-            self._inode = fs._Get_Inode(self.name, self.parent)
-        except ResourceNotFound:
-            self._inode = None
+        self._inode = inode
 
     def next(self):
         data = self.readline()
@@ -50,3 +39,23 @@ class DirEntry(object):
 
     def __iter__(self):
         return self
+
+
+class DirEntry(Inode):
+    '''
+    classdocs
+    '''
+
+    def __init__(self, fs):
+        '''
+        Constructor
+        '''
+        self.parent, self.name = split(self.path)
+
+        try:
+            self.parent = fs._Get_Inode(self.parent)
+            inode = fs._Get_Inode(self.name, self.parent)
+        except (ParentDirectoryMissing, ResourceNotFound):
+            inode = None
+
+        Inode.__init__(self, fs, inode)
